35784d7822d56d155c64bd2128065822932fe539
GlitzyIsBored <dragonstudio209@gmail.com>
2026-03-09 22:35:18 +1100

    woah games

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,18 +2,18 @@ const Grid = () => {
   return (
     <section id="box-container">
       <div class="box box-1">
-        <p style={{ fontWeight: "bold" }}>Coffee</p>
-        <p>Coffee</p>
-        <p>Coffee</p>
-        <p>Coffee</p>
-        <p>Coffee</p>
+        <p style={{ fontWeight: "bold" }}>Beloved games</p>
+        <p>Devil May Cry</p>
+        <p>Final Fantasy XV</p>
+        <p>Sonic Unleashed</p>
+        <p>God Of War</p>
       </div>
       <div class="box box-2">
-        <p style={{ fontWeight: "bold" }}>Coffee</p>
-        <p>Coffee</p>
-        <p>Coffee</p>
-        <p>Coffee</p>
-        <p>Coffee</p>
+        <p style={{ fontWeight: "bold" }}>Beloved games</p>
+        <p>Infinity Nikki</p>
+        <p>Yume Nikki</p>
+        <p>Resonance of Fate</p>
+        <p>NieR: Automata</p>
       </div>
       <div class="box box-3">
         <p style={{ fontWeight: "bold" }}>Emacs</p>
